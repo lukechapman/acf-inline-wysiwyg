6ae3f375a7650bbc4b379e54a9b0751d47b20772
Adam George <github@adamgeorge.com>
2017-03-30 10:20:25 +1100

    Remove bits that weren't needed/used

--- includes/js/inline-wysiwyg.js
+++ includes/js/inline-wysiwyg.js
@@ -2,9 +2,6 @@
 Version: 1.0.2
 */
 
-var acf_medium_editors = {};
-var acf_medium_editor_timeout = false;
-
 (function($){
 
 	var options = {
@@ -80,53 +77,6 @@ var acf_medium_editor_timeout = false;
 			});
 			return;
 		}
-
-		var $container;
-		var $static = false;
-		var $align = 'left';
-
-		if (navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1)  {
-			$static = true;
-			$container = null;
-		} else {
-			$data.closest('.acf-input').prepend('<div id="medium-editor-container-'+$uniqid+'"></div>');
-			$container = document.getElementById('medium-editor-container-'+$uniqid);
-		}
-
-		$static = false;
-		$container = null;
-
-    /*
-		var $buttons = decodeURIComponent($data.data('buttons'));
-		var $buttons = JSON.parse(decodeURIComponent($data.data('buttons')));
-		var $extensions = JSON.parse(decodeURIComponent($data.data('extensions')));
-		var $extension_object = {};
-		$custom_buttons = {};
-		for (i=0; i<$extensions.length; i++) {
-			$custom_buttons[$extensions[i]['name']] = new MediumButton($extensions[i]['settings']);
-		}
-		var $placeholder = $data.data('placeholder');
-		$options =  JSON.parse(decodeURIComponent($data.data('options')));
-
-		var $object = {
-			toolbar: {
-				buttons: $buttons,
-				static: $static,
-				align: $align,
-				relativeContainer: $container
-			},
-			extensions: $custom_buttons,
-			placeholder: {
-				text: $placeholder,
-				hideOnClick: false
-			},
-			elementsContainer: $container
-		};
-
-		for (i in $options) {
-			$object[i] = $options[i];
-		}
-    */
 
 		var editor = new MediumEditor($selector, options);
 
